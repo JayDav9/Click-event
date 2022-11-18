ab13a0ba074283c5754ade5c7cc798a7b3fe4391
JD <jaydav95@outlook.com>
2022-11-18 08:57:23 -0600

    Click events

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React, {useState} from "react"
 
 function App() {
+
+  const [coordinates, setCoordinates] = useState([]);
+   const [popped, setPopped] = useState([]);
+
+  function handleCircle (e)
+  {
+    const {clientX,clientY} = e;
+   
+
+    setCoordinates([...coordinates, {x: clientX, y: clientY, 
+      color:(  "#" + Math.floor(Math.random()*16777215).toString(16)),
+    height: (Math.floor(100+200 * Math.random())),
+  }])
+  }
+
+  function undo(e){
+   let poppedCoordinates = [...coordinates];
+    let thisPop = poppedCoordinates.pop();
+    let poppedList = ([...popped, thisPop]);
+    setPopped(poppedList);
+   setCoordinates(poppedCoordinates);
+   console.log(poppedCoordinates)
+
+
+  }
+  function redo(e){
+    if (popped.length < 1)
+    return;
+    let redo = popped.pop();
+    setCoordinates([...coordinates, redo]);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    
+    <button onClick={undo}>Undo </button>
+    <button onClick={redo}>Redo</button>
+    <div className="App" onClick = {handleCircle}>
+
+
+  
+      {
+        coordinates.map((coordinate,idx)=>
+        {
+       
+          return <div key = {idx} className = "coordinate" style = {{left : coordinate.x-50 + "px",
+           top : coordinate.y-50 + "px",
+           backgroundColor : coordinate.color,
+           height: coordinate.height,
+          width: coordinate.height,
+          borderRadius: "50%"
+          }}> </div>
+        })
+     
+      }
+
+
     </div>
-  );
+    </>
+    );
 }
 
 export default App;
